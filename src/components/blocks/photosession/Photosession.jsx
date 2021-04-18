5356f1283363eabd3fe09709921abb6f2f74a76e
Ramashka Luba <lromashko911@gmail.com>
2021-04-18 12:53:23 +0300

    RatingBlock, Photosession

--- src/components/blocks/photosession/Photosession.jsx
+++ src/components/blocks/photosession/Photosession.jsx
@@ -12,40 +12,36 @@ function Photosession(props) {
     return (
         <div className={s.photosession}>
             <div className={s.container}>
-                <h2 className={s.title}>Фотосессия в вертолете</h2>
-                <div className={s.video}></div>
-                <div className={s.wrap}>
-                    <p className={s.text}>Полет на вертолете это прекрасная возможность получить новые ощущения! А наш замечательный фотограф запечатлит лучшие моменты!</p>
-                    <Details
-                        src={Watch}
-                        alt="watch"
-                        value="3 часа съемки"
-                    />
-                    <Details
-                        src={Camera}
-                        alt="watch"
-                        value="Более 50 фотографий"
-                    />
-                    <Details
-                        src={Map}
-                        alt="watch"
-                        value="Москва"
-                    />
-                    <Details
-                        src={Chat}
-                        alt="watch"
-                        value="Готовы ответить на любые вопросы"
-                    />
-                    <RatingBlock/>
-                    
+                <div className={s.inner}>
+                    <h2 className={s.title}>Фотосессия в вертолете</h2>
+                    <div className={s.video}></div>
+                    <div className={s.wrap}>
+                        <p className={s.text}>Полет на вертолете это прекрасная возможность получить новые ощущения! А наш замечательный фотограф запечатлит лучшие моменты!</p>
+                        <Details
+                            src={Watch}
+                            alt="watch"
+                            value="3 часа съемки"
+                        />
+                        <Details
+                            src={Camera}
+                            alt="watch"
+                            value="Более 50 фотографий"
+                        />
+                        <Details
+                            src={Map}
+                            alt="watch"
+                            value="Москва"
+                        />
+                        <Details
+                            src={Chat}
+                            alt="watch"
+                            value="Готовы ответить на любые вопросы"
+                        />
+                        <RatingBlock/>
+                    </div>
                 </div>
-
-                
-
+            </div>
         </div>
-        </div>
-        
-        
     );
 }
 
--- src/components/ratingBlock/RatibgBlock.jsx
+++ src/components/ratingBlock/RatibgBlock.jsx
@@ -9,7 +9,7 @@ function RatingBlock (props) {
         <div className={s.wrap} style={props.style}>
             <img className={s.icon}  src={Cup} alt="cup"/>
             <div className={s.content}>
-                <p className={s.text}>Более 150 отзывов с оценкой</p>
+                <p className={s.text}>Более <span>50</span> отзывов с оценкой</p>
                 <img className={s.star}  src={Star} alt="cup"/>
             </div>           
         </div>  
