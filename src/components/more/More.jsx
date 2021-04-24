87c31fc2dab88280f56d5fe59d27e0f840864366
Ramashka Luba <lromashko911@gmail.com>
2021-04-24 12:25:01 +0300

    More, Photosession,Title

--- src/components/more/More.jsx
+++ src/components/more/More.jsx
@@ -6,13 +6,11 @@ import Arrow from './../../assets/img/Gallery/Arrow.png'
 function More(props) {
     return (
         <div className={s.more}>
-            <div className={s.container}>
                 <div className={s.inner}>
                     <img className={s.arrow} src={Arrow} alt="arrow" />
                     <a className={s.link}>{props.moreText}</a>
                     <img className={s.arrow} src={Arrow} alt="arrow" />
                 </div>
-            </div>
         </div>
     );
 }
