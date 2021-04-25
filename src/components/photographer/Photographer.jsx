5ac18e94c2baf7ffa05a59a60c34817dc846676e
Ramashka Luba <lromashko911@gmail.com>
2021-04-25 10:50:46 +0300

    Profile

--- src/components/photographer/Photographer.jsx
+++ src/components/photographer/Photographer.jsx
@@ -1,24 +1,19 @@
 import s from "./Photographer.module.scss";
+import User from "./../../assets/img/Profile/Ellipse.png";
+import Like from "./../../assets/img/Profile/Vector.png";
 
 
 function Photographer (props) {
     return (
         <div className={s.photographer}>
-            <img className={s.user} src="" alt=""/>
-            <div className={s.info}></div>
-        </div>
-
-
-
-
-
-        // <div className={s.wrap} style={props.style}>
-        //     <img className={s.icon}  src={Cup} alt="cup"/>
-        //     <div className={s.content}>
-        //         <p className={s.text}>Более <span>50</span> отзывов с оценкой</p>
-        //         <img className={s.star}  src={Star} alt="cup"/>
-        //     </div>           
-        // </div>  
+            <img className={s.photo} src={User} alt="photo"/>
+            <div className={s.info}>
+                <p className={s.mane}>Марина Иванова</p>
+                <span className={s.text}>Фотограф</span>
+                <a className={s.link} >Показать больше информации о фотографе</a>
+            </div>
+            <img className={s.like} src={Like} alt="like"/>
+        </div> 
     );
 }
 
