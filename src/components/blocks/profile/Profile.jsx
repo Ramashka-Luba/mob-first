0dba5f7f9bdf13a72668d6e3d2251a0123e4ccf0
Ramashka Luba <lromashko911@gmail.com>
2021-05-19 22:26:48 +0300

    Profile, Description

--- src/components/blocks/profile/Profile.jsx
+++ src/components/blocks/profile/Profile.jsx
@@ -1,5 +1,6 @@
 import s from './Profile.module.scss';
 import Photographer from './../../photographer/Photographer';
+import Description from '../../description/Description';
 
 
 function Profile () {
@@ -7,6 +8,18 @@ function Profile () {
         <div className={s.profile}>
             <div className={s.container}>
                 <Photographer/>
+                <Description
+                    title="Об организаторе:"
+                    text1="Всем привет! Я фотограф Мария! И по мимо фотосъемки я так же являюсь членом группы экспертов туристической отрасли, которые хорошо разбираются 
+                            в маркетинге, операциях 
+                            и услугах обслуживания клиентов, специализирующихся 
+                            на совместном использовании 
+                            и частных турах."
+                    text2="Сначала мы (я и моя команда) изучаем интересы клиентов, а затем придумываем подходящее решение, которое соответствует бюджету и планам клиента. 
+                            Мы никогда не зависим 
+                            от субпоставщиков 
+                            и не свяжемся…"
+                />
             </div>
         </div>
     );
--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,11 +1,17 @@
 import s from "./Description.module.scss";
+import Arrow from "./../../assets/img/Gallery/Arrow.png"
 
 function Description (props) {
     return (
-        <div className={s.descripttion}>
-            <h3 className={s.title}></h3>
-            <p className={s.text}></p>
-
+        <div className={s.descripttion} style={props.style}>
+            <h3 className={s.title}>{props.title}</h3>
+            <p className={s.text}>{props.text1}</p>
+            <p className={s.text}>{props.text2}</p>
+            <div className={s.wrap}>
+                <a className={s.link}>Читать еще</a>
+                <img className={s.arrow} src={Arrow} alt="arrow" ></img>
+            </div>
+            
         </div>
     );
 }
